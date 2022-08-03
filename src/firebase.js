0468e1013c59046cc29bbaca6f14bf03cf4dbc64
syee19 <syee19@naver.com>
2022-08-04 01:38:28 +0900

    2장 완료 커밋

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEqxsB0YNwWS9m6JRNmTWSmbfX5KdzbYY",
+  authDomain: "nwitter-c8fe8.firebaseapp.com",
+  projectId: "nwitter-c8fe8",
+  storageBucket: "nwitter-c8fe8.appspot.com",
+  messagingSenderId: "671877531010",
+  appId: "1:671877531010:web:d001ae6affb64170083b9b",
+};
+
+export default firebase.initializeApp(firebaseConfig);
