57e54b00d5e4a15715f5f16b2375daeb598175bf
syee19 <syee19@naver.com>
2022-08-04 02:15:15 +0900

    3장 완료 커밋

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAEqxsB0YNwWS9m6JRNmTWSmbfX5KdzbYY",
-  authDomain: "nwitter-c8fe8.firebaseapp.com",
-  projectId: "nwitter-c8fe8",
-  storageBucket: "nwitter-c8fe8.appspot.com",
-  messagingSenderId: "671877531010",
-  appId: "1:671877531010:web:d001ae6affb64170083b9b",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
